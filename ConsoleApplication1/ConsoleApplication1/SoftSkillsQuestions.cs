4ecc1504a234d94e8eba0288759845d8fd7239a8
mchaffin <mchaffin@ebay.com>
2016-04-27 11:28:07 -0700

    Soft skills questions: initial responses

--- ConsoleApplication1/ConsoleApplication1/SoftSkillsQuestions.cs
+++ ConsoleApplication1/ConsoleApplication1/SoftSkillsQuestions.cs
@@ -1,20 +1,77 @@
 ﻿/*
-    1.  How do you typically handle yourself in a fast-paced environment?
+1.  How do you typically handle yourself in a fast-paced environment?
 
-    2.  How would you describe your work pace: fast, moderate, slow, or if it varies and under what circumstances?
+	I'm going to answer this with a question. What does your company/team mean by a "fast-paced envrionment"? 
+	Do you mean that you have a well designed system and do a well planned release every 2 weeks? 
+	Or do you mean that shortcuts are being taken for the sake of some arbitrary schedule and so you're always scrambling?
 
-    3.  What are your strengths? Why?
 
-    4.  What are your weaknesses? Why?
 
-    5.  How do you handle a heavy workload with many deadlines?
+2.  How would you describe your work pace: fast, moderate, slow, or if it varies and under what circumstances?
 
-    6.  In all jobs there are heavy periods and light periods. What do you do in such cases?
+	It varies. Depends on the tasks at hand. 
+	Design tasks may appear to be slow going, but that's likely because there's a lot of thinking going on. 
+	Implementation tasks are usually at a pretty moderate pace. Going too fast results in bugs, overlooking edge cases, and such.
 
-    7.  You are given several rush projects to be completed in a short period of time. How would you establish priorities? Why?
 
-    8.  How would you describe your organizational abilities?
 
-    9.  How would you describe your attendance and punctuality habits?How would you describe your work habits? How would you describe your own performance standards?
+3.  What are your strengths? Why?
+
+	Experience based intuition. I often "know" a solution very quickly. Its usually right or a long way down the path to being right.
+	Then, I have to figure out how to justify that solution to my team. Usually, this involves providing anecdotal evidence, 
+	drawing on whiteboards, looking up resources, and sometimes prototyping.
+
+
+
+4.  What are your weaknesses? Why?
+
+	Wearing my heart on my sleeve. You'll generally know how I feel about a project, solution, or whatever is going on. 
+	I sometimes have a hard time hiding frustrations.
+
+
+
+5.  How do you handle a heavy workload with many deadlines?
+
+	Prioritization and setting expectations.
+
+
+
+6.  In all jobs there are heavy periods and light periods. What do you do in such cases?
+
+	Appreciate the down times. They sometimes don't come often enough. 
+	Take advantage of the time to ramp up on anything new that you'll need for upcoming projects, perhaps focus on some team building.
+	
+
+
+
+7.  You are given several rush projects to be completed in a short period of time. How would you establish priorities? Why?
+
+	Using Eisenhower's Urgent/Important Principle. Work on Important tasks vs. not important ones. Urgent vs. not urgent ones.  
+	So priorities would be:
+	1. Important and Urgent tasks
+	2. Important and Not Urgent tasks (hopefully, most of our time is spent here)
+	3. Not Important, but Urgent tasks
+	4. Not Important and Not Urgent tasks (in a work environment, these are unlikely to ever be completed)
+	
+	An exception to this guideline would be if there is some task that I can do relatively quickly to unblock someone else. 
+	Those tasks are often not important to my success and should fall lower in priority, but they may be or contribute toward 
+	something important to the team's success and so I would bump them up.
+
+
+
+8.  How would you describe your organizational abilities?
+
+	I am a PM in developer's clothing.
+
+
+
+9.  How would you describe your attendance and punctuality habits?How would you describe your work habits? How would you describe your own performance standards?
+
+	I have high standards for myself. 
+	I loathe duplication of effort (or code); therefore, I strive for good design to reduce duplication and increase readability.
+
+	Side note: 
+		Attendance and punctuality? Really? Are these questions you would ask a senior engineer?  
+		If you don't show up for work and/or don't get to meetings on time, you aren't going to last anywhere.
 
 */
